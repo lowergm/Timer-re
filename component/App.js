18b639e05a0dc1b45fa91342bf644be05842903f
lowergm <92257300+lowergm@users.noreply.github.com>
2024-10-31 00:03:29 -0300

    Update and rename Contador.js to App.js

---
+++ component/App.js
@@ -0,0 +1,48 @@
+export default {
+  data() {
+    return {
+      horas: 0,
+      minutos: 0,
+      segundos: 0,
+      intervalo: null,
+      rodando: false,
+    }
+  },
+  methods: {
+    aumentar() {
+      if (!this.rodando){
+        this.rodando = true;
+        this.segundos = 0;
+        this.intervalo = setInterval(()=>{
+          this.segundos += 1;
+          if (this.segundos == 60){
+            this.segundos = 0;
+            this.minutos += 1;
+          }
+  
+          if (this.minutos == 60){
+            this.minutos = 0;
+            this.horas += 1;
+          }
+        }, 1000);
+      }
+    },
+    parar() {
+      if (this.rodando){
+        clearInterval(this.intervalo);
+      this.rodando = false;
+      }
+    }
+  },
+  beforeDestroy() {
+    this.parar();
+  },
+  template: `
+  <div>
+    <h1>Timer-re</h1>
+    <p><span>{{ String(horas).padStart(2, "0") }}:{{ String(minutos).padStart(2, "0") }}:{{ String(segundos).padStart(2, "0") }}</span></p>
+    <button @click="aumentar">Iniciar</button>
+    <button @click="parar">Parar</button>
+  </div>
+  `,
+}

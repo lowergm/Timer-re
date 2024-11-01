a6391bf5ca6a742f8f7b74959d47f72349419795
lowergm <92257300+lowergm@users.noreply.github.com>
2024-11-01 10:04:41 -0300

    Update App.js

--- components/App.js
+++ components/App.js
@@ -5,12 +5,16 @@ export default {
       minutos: 0,
       segundos: 0,
       intervalo: null,
+      mostrarIniciar: true,
+      mostrarControls: false,
       rodando: false,
     }
   },
   methods: {
     iniciar() {
       if (!this.rodando){
+        this.mostrarControls = true;
+        this.mostrarIniciar = false;
         this.rodando = true;
         this.segundos = 0;
         this.minutos = 0;
@@ -36,6 +40,9 @@ export default {
       }
     },
     resetar() {
+      this.mostrarIniciar = true;
+      this.mostrarControls = false;
+      this.parar();
       this.rodando = false;
       this.horas = 0;
       this.minutos = 0;
@@ -49,9 +56,9 @@ export default {
   <div>
     <h1>Timer-re</h1>
     <p><span>{{ String(horas).padStart(2, "0") }}:{{ String(minutos).padStart(2, "0") }}:{{ String(segundos).padStart(2, "0") }}</span></p>
-    <button @click="iniciar">Iniciar</button>
-    <button @click="parar">Parar</button>
-    <button @click="resetar">Resetar</button>
+    <button v-if="mostrarIniciar" @click="iniciar">Começar</button>
+    <button v-if="mostrarControls" @click="parar">Parar</button>
+    <button v-if="mostrarControls" @click="resetar">Resetar</button>
   </div>
   `,
 }
